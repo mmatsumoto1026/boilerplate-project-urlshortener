e3352cbc4d96be691f27246a0fe25d377c08bed0
mmatsumoto1026 <mmatsumoto1026@gmail.com>
2023-10-16 20:43:44 +0900

    fix: remove the unused package

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const dns = require("dns")
 const app = express();
 
 const mongoose = require("mongoose");
-const { url } = require('inspector');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
